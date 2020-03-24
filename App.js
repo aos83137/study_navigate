c641734f18a3c2c5d685729b4786f3ad5790fedc
YongSeok <qazwsx1455@gmail.com>
2020-03-24 21:01:09 +0900

    tap 아이콘 + 아이콘 배지(알람용) 추가

--- App.js
+++ App.js
@@ -4,7 +4,41 @@ import { Button, View, Text,TextInput, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+// import { Ionicons } from '@expo/vector-icons';
 
+function IconWithBadge({ name, badgeCount, color, size }) {
+  return (
+    <View style={{ width: 24, height: 24, margin: 5 }}>
+      <Icon name={name} size={size} color={color} />
+      {badgeCount > 0 && (
+        <View
+          style={{
+            // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
+            position: 'absolute',
+            right: -6,
+            top: -3,
+            backgroundColor: 'red',
+            borderRadius: 6,
+            width: 12,
+            height: 12,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+            {badgeCount}
+          </Text>
+        </View>
+      )}
+    </View>
+  );
+}
+
+function HomeIconWithBadge(props) {
+  // React Context API, Redux, MobX 또는 이벤트 이미 터와 같은 다른 방법으로 badgeCount를 전달해야합니다.
+  return <IconWithBadge {...props} badgeCount={3} />;
+}
 
 function HomeScreen() {
   return (
@@ -31,15 +65,37 @@ function SettingsScreen() {
 }
 
 const RootStack = createStackNavigator();
-const MainTopStack = createBottomTabNavigator();
+const MainTabStack = createBottomTabNavigator();
 
 function MainStackScreen(){
   return (
-    <MainTopStack.Navigator>
-      <MainTopStack.Screen name="Home" component={HomeScreen}/>
-      <MainTopStack.Screen name="Info" component={InfoScreen}/>
-      <MainTopStack.Screen name="Setting" component={SettingsScreen}/>
-    </MainTopStack.Navigator>
+    <MainTabStack.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon : ({ focused, color , size}) => {
+          if (route.name === 'Home'){
+            return(
+              <Icon name = {focused ? 'home' : 'home'}  size={size} color={color}/>
+            );
+          } else if (route.name==='Info'){
+            return(
+              <HomeIconWithBadge name = {focused ? 'info' : 'info-outline'}  size={size} color={color}/>
+            );
+          } else if(route.name==='Setting'){
+            return(
+              <Icon name ={focused ? 'settings-applications' : 'settings'}  size={size} color={color}/>
+            );
+          }
+        },
+      })}
+      tabBarOptions={{ 
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <MainTabStack.Screen name="Home" component={HomeScreen}/>
+      <MainTabStack.Screen name="Info" component={InfoScreen}/>
+      <MainTabStack.Screen name="Setting" component={SettingsScreen}/>
+    </MainTabStack.Navigator>
   );
 }
 
