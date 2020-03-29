f38eab0dd4a18a06981ebf4dc3d437f58b741b2a
YongSeok <qazwsx1455@gmail.com>
2020-03-29 15:28:17 +0900

    일단 현시점에서 mapView는 뜸 실행 순서가 install 하고 그냥 link 해주니까 됨

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,28 @@
 import React , {Component} from 'react';
 import {Text ,View, ScrollView, StyleSheet} from 'react-native';
 
+import MapView, {Marker,PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+
+
 export default class HomeScreen extends Component{
     render(){
         return(
             <View style={{flex:1, justifyContent: 'center', alignItems:'center' }}>
-                <Text>Home!!!</Text>
-                <Text>Map create</Text>  
+                {/* <Text>Home!!!</Text>
+                <Text>Map create</Text>   */}
+                <MapView
+                    provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                    style={styles.map}
+                ></MapView>
              </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    map: {
+        ...StyleSheet.absoluteFillObject,
+        //(position: 'absolute', left: 0, right: 0, top: 0, bottom: 0)
+        zIndex:1,
+    }
+})
